4c504ba43c55f3946bc26261415a60e50a0946e7
Balakrishna <eerotubalakrishna@gmail.com>
2024-06-13 11:39:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {Route, Switch,BrowserRouter} from 'react-router-dom'
+
+import Home from './components/Home'
+import LoginForm from './components/LoginForm'
+
+import './App.css';
+
+const  App=()=>(
+  <BrowserRouter> 
+  <Switch>
+  <Route exact path="/login" component={LoginForm}/>
+    <Route exact path="/" component={Home}/>
+     
+  </Switch>
+  </BrowserRouter>
+)
+
+export default App;
---
+++ src/components/Home/index.js
@@ -0,0 +1,25 @@
+import {Component} from 'react'
+import './index.css'
+
+class Home extends Component{
+    render(){
+        return(
+            <div className="home-container">
+                <div className="homo-logos-container">
+                 <img src="https://i.pinimg.com/280x280_RS/a0/32/a3/a032a368dff26b4677c715a8856ef6fd.jpg" className='img'/>
+                <img src="https://i.pinimg.com/564x/58/3b/f1/583bf1744b9419813a94323c68f23df9.jpg" className='img'/>
+                <img src="https://i.pinimg.com/564x/7c/1d/17/7c1d1781450402e49010726af8ba56fa.jpg" className='img'/>
+                <img src="https://i.pinimg.com/564x/5d/b9/cb/5db9cb7eb89795758c5cfd66876e6837.jpg" className='img'/>
+                <img src="https://i.pinimg.com/564x/f4/34/e3/f434e30f1c5a170fc4700661cc99c3ab.jpg" className='img'/>
+                <img src="https://i.pinimg.com/564x/ae/5f/e3/ae5fe3dc423e44c0ddbef5dc64fa356b.jpg" className='img'/>
+                <img src="https://i.pinimg.com/736x/8d/ec/e1/8dece15cc40aaf66ed47f6591b639d06.jpg" className='img'/>
+                <img src="https://i.pinimg.com/564x/39/34/bb/3934bbe8d1d7ca41fbf451357538dd55.jpg" className='img'/>
+
+                 
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home
